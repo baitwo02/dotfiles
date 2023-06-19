f79039e807d92ccab2a5dce32918d5fa6d450f21
baitwo02 <baitwo02@gmail.com>
2023-06-19 19:30:21 +0800

    add nvim config and change the install script
    添加了nvim配置文件
    并且更改了install.py脚本, 更改了软链部分, 添加了必备软件的安装

--- install.py
+++ install.py
@@ -1,26 +1,38 @@
 import os
 
-home_directory = os.environ["HOME"]
+# 创建软链
+path_homoe = os.environ["HOME"]
 user_name  = os.environ["LOGNAME"]
 
-path_config = home_directory + "/.config"
-path_dotfiles = home_directory + "/dotfiles"
+path_cfg = path_homoe + "/.config"
+path_dot = path_homoe + "/dotfiles"
 
-config_folders = [
+cfgs = [
+    "kwalletrc",
     "fish",
     "ranger",
     "kitty",
     "alacritty",
     "tmux",
-    "kwalletrc",
     "hypr",
     "waybar",
     "wofi",
     "nvim",
 ]
-for config_folder in config_folders:
-    os.system("rm -rf %s/%s" % (path_config, config_folder))
-    os.symlink("%s/%s" % (path_dotfiles, config_folder),
-               "%s/%s" % (path_config, config_folder))
+for cfg in cfgs:
+    os.system(f"rm -rf {path_cfg}/{cfg}")
+    os.symlink(f"{path_dot}/{cfg}", f"{path_cfg}/{cfg}")
 
+# 安装需要的软件
+needed_packages = [
+    "tar",
+    "gzip",
+    "unzip",
+    "curl", 
+    "wget",
+    "npm",
+    "nodejs",
+]
 
+for needed_package in needed_packages:
+    os.system(f"yay --noconfirm -S {needed_package}")
