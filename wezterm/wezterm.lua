3354a516785378ae6adb7a02aa4e21060ee72bf9
baitwo02 <baitwo02@gmail.com>
2024-11-28 10:19:35 +0800

    change margin of hyprland
    change the interval to 20

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,6 +10,9 @@ local config = wezterm.config_builder()
 config.window_background_opacity = 0.6
 config.font = wezterm.font 'Hack Nerd Font Mono'
 config.font_size = 14
+config.min_scroll_bar_height = "50px"
+config.scroll_to_bottom_on_input = true
+config.alternate_buffer_wheel_scroll_speed = 1
 
 -- and finally, return the configuration to wezterm
 return config
