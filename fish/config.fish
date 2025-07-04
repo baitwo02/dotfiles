bdeebc8fae8ef239b180ebe233d38dedb7c63ac5
baitwo02 <baitwo02@gmail.com>
2025-07-04 13:02:34 +0800

    modified some ui, remove wpaperd from start software

--- fish/config.fish
+++ fish/config.fish
@@ -2,13 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set GOPATH $HOME/go
-set GOBIN $GOPATH/bin
 set CARGOBIN $HOME/.cargo/bin
 set PATH $HOME/.local/bin $PATH
-set PATH $GOBIN $PATH
-set PATH $CARGOBIN $PATH
 
-alias "sudo" "sudo -E"
-alias "nano" "nvim"
+alias sudo "sudo -E"
+alias nano nvim
 
+starship init fish | source
